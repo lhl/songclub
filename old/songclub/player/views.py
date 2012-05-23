f484c0ffd9db1599f0a85a156cc2cf090580d7ba
lhl <lhl@randomfoo.net>
2012-05-23 06:16:47 -0500

    Moved old django stuff out

--- old/songclub/player/views.py
+++ old/songclub/player/views.py
@@ -7,8 +7,8 @@ from django.shortcuts import render_to_response
 from player.models import File
 
 @login_required
-def index(request):
-  latest_folders = File.objects.filter(type='folder', mp3count__isnull=False).values('id', 'locationsha1', 'location', 'path', 'name', 'mtime')[:10]
+def index(request, count=10):
+  latest_folders = File.objects.filter(type='folder', mp3count__isnull=False).values('id', 'locationsha1', 'location', 'path', 'name', 'mtime')[:count]
   return render_to_response('index.html', {'latest_folders': latest_folders})
 
 @login_required
@@ -18,25 +18,27 @@ def file(request, hash):
     return HttpResponseNotFound("You tried to request a folder.")
     pass
   else:
+    # Best solution until Django 1.1, see: 
+    # http://code.djangoproject.com/ticket/2131
+    # http://code.djangoproject.com/ticket/7581
+    # http://code.djangoproject.com/ticket/7894
     mimetype = 'audio/mpeg'
-
     response = HttpResponse(FileWrapper(open(f['location'])), content_type=mimetype)
     response['Content-Length'] = f['size']
     return response
 
-    ''' Has to read the whole file...
-    response = HttpResponse()
-    response['Content-Type'] = mimetype
-    response['Content-Length'] = f['size']
-    response.content = open(f['location']).read()
-    return response
-    '''
-
-
-"""
+@login_required
+def browse(request, hash):
+  f = File.objects.filter(locationsha1=hash).values('location', 'type', 'name', 'path', 'mimetype', 'mtime', 'size')[0]
+  if f['type'] == 'folder':
+    # Return Listing
+    """
     files = File.objects.filter(path=f['location'][:-1]).values('location', 'type', 'name', 'path', 'mimetype', 'mtime')
     for f in files:
       print f
     return HttpResponse("Hello, world. You're looking for a file: %s" % f['location'])
+    """
+    pass
   else:
-"""
+    # Not Folder
+    pass
