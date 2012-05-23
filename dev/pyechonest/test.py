5232be48a28c2d7c5123a860011d37d1b26ffce0
Gabe Hollombe <gabe@avantbard.com>
2012-05-23 22:05:28 +0700

    progress with enmfp

--- dev/pyechonest/test.py
+++ dev/pyechonest/test.py
@@ -1,7 +1,10 @@
 """
-This expects an .api_keys file with the following section:
+This expects an .config file with the following sections:
 [API Keys]
 api_key=1234567890
+
+[TOOLS]
+enmfp_codegen=/path/to/enmfp/codegen
 """
 import configparser
 config = configparser.ConfigParser()
@@ -11,4 +14,12 @@ api_key = config['API Keys']['api_key']
 from pyechonest import config 
 config.ECHO_NEST_API_KEY=api_key
 
+import subprocess
+import json
+
+# Get the JSON ENMFP from an MP3
+def get_enmfp_from_mp3(mp3_path):
+  json_str = subprocess.check_output([config['Tools']['enmfp_codegen'], mp3_path, 10, 10]).decode('utf-8')
+  result = json.loads(json_str)
+  return result
 
