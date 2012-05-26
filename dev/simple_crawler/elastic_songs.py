3065634e412387b4a3dbc6c9d5c8404a3de40b39
Gabe Hollombe <gabe@avantbard.com>
2012-05-26 14:10:15 +0700

    common file for easier working with repl, and comment about using elasticsearch with pyes

--- dev/simple_crawler/elastic_songs.py
+++ dev/simple_crawler/elastic_songs.py
@@ -10,15 +10,15 @@ redis_client = redis.Redis(host='localhost', port=6379, db=0)
 
 def reset_indicies():
   try:
-    es_conn.delete_index('songs-index')
-    es_conn.delete_index('files-index')
-    es_conn.delete_index('artists-index')
+    es_conn.delete_index('songs')
+    es_conn.delete_index('files')
+    es_conn.delete_index('artists')
   except:
     pass
 
-  es_conn.create_index("songs-index")
-  es_conn.create_index("files-index")
-  es_conn.create_index("artists-index")
+  es_conn.create_index("songs")
+  es_conn.create_index("files")
+  es_conn.create_index("artists")
 
 
 def repopulate_from_redis():
@@ -34,15 +34,25 @@ def repopulate_from_redis():
     artist_doc = redis_client.get(artist_key)
     add_artist(artist_doc)
 
+  es_conn.refresh()
 
 
-def add_file(file_doc):
-  es_conn.index(file_doc, 'files-index', 'file')
+
+def add_file(file_doc, bulk=True):
+  es_conn.index(file_doc, 'files', 'file', bulk=bulk)
 
 
-def add_song(song_doc):
-  es_conn.index(song_doc, 'songs-index', 'song')
+def add_song(song_doc, bulk=True):
+  es_conn.index(song_doc, 'songs', 'song', bulk=bulk)
 
 
-def add_artist(artist_doc):
-  es_conn.index(artist_doc, 'artists-index', 'artist')
+def add_artist(artist_doc, bulk=True):
+  es_conn.index(artist_doc, 'artists', 'artist', bulk=bulk)
+
+
+""" 
+Example of how to query elastic_search
+q = TermQuery("title", "slow")
+qr = es_conn.search(q.search())
+qr.hits
+"""
