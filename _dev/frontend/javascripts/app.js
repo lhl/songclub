f4cb8ace1630ee6d05461c82c621d992adbfe2fb
Ganesh Shankar <ganesh@ganeshshankar.com>
2012-05-27 13:45:23 +0700

    making a sample app based on tut

--- _dev/frontend/javascripts/app.js
+++ _dev/frontend/javascripts/app.js
@@ -32,18 +32,18 @@ App.filesController = Em.ArrayController.create({
     var files = this;
     var url = 'magic_path_to_get_files?'
     url += query;
-    $.getJSON(url,function(data){
-      files.set('content', []);
-      $(data).each(function(index,value){
-        var t = App.Song.create({
-          id: value.id,
-          en_song_id: value.en_song_id,
-          en_artist_id: value.en_artist_id,
-          path: value.path
-          files.pushObject(t);
-        })
-      });
-    });
+    // $.getJSON(url,function(data){
+    //   files.set('content', []);
+    //   $(data).each(function(index,value){
+    //     var t = App.Song.create({
+    //       id: value.id,
+    //       en_song_id: value.en_song_id,
+    //       en_artist_id: value.en_artist_id,
+    //       path: value.path
+    //       files.pushObject(t);
+    //     })
+    //   });
+    // });
   }
 });
 
