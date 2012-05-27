628e22efa0933d9fedbdae8f384ece6017a17781
lhl <lhl@randomfoo.net>
2012-05-27 05:03:57 -0500

    Temp edit - need to change to FB vs GAuth

--- handlers/auth.py
+++ handlers/auth.py
@@ -9,18 +9,20 @@ class AuthLoginHandler(BaseHandler, tornado.auth.GoogleMixin):
     if self.get_argument('openid.mode', None):
       self.get_authenticated_user(self.async_callback(self._on_auth))
       return
-    self.authenticate_redirect('http://admin.lensley.com/login')
+    self.authenticate_redirect('/login')
                                                                                    
   def _on_auth(self, user):
     ## auth fail
     if not user:
       raise tornado.web.HTTPError(500, 'Google auth failed')
 
+    '''
     if user.has_key('email'):
       if not re.search(r'@lensley.com$', user['email']):
         raise tornado.web.HTTPError(500, 'Must be lensley.com user')
     else:
       raise tornado.web.HTTPError(500, 'Must be lensley.com user')
+    '''
 
     '''{'locale': u'en-us', 'first_name': u'Leonard', 'last_name': u'Lin', 'name': u'Leonard Lin', 'email': u'lhl@lensley.com'}'''
     identity = self.get_argument('openid.identity', None)
